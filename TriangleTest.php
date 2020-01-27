576ffbc6e4763dc51edae4be686853835d3bca98
rakib-09 <rakib.rumi09@gmail.com>
2020-01-27 23:34:47 +0600

    added triagle error solving issue

---
+++ TriangleTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rakib
+ * Date: 1/27/2020
+ * Time: 10:34 PM
+ */
+
+class TriangleTest {
+    public function index()
+    {
+        //get 3 sides values
+        $first = readline("first value: ");
+        $sec = readline("second value: ");
+        $third = readline("third value: ");
+        //assign them
+        //get angle A
+        //a^2 = b^2+c^2+2bcCOSA
+
+        //get angle B
+        //SINA/a = SINB/b
+        //the rest is C
+        //C = 180 - (A+B)
+    }
+}
